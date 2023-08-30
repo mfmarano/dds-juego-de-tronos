1e20be2385c8fec77c9f67c3631bd50b0ae1af80
mfmarano <mmarano@frba.utn.edu.ar>
2023-08-30 00:29:19 -0300

    agregar Casa

--- src/main/java/Casa.java
+++ src/main/java/Casa.java
@@ -2,29 +2,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.util.Collection;
 
 @Entity
 public class Casa {
+  
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String nombre;
   private int patrimonio;
   private int anioFundacion;
+  @ManyToOne
   private Casa vasallaDe;
+  @OneToMany
   private Collection<FuerzaMilitar> fuerzasMilitares;
+  @ManyToOne
   private Lugar origen;
 
   public String nombreLugarOrigen() {
     return origen.getNombre();
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Long getId() {
-    return id;
   }
 }
